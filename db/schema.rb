3f8feacb3d2d826bda95d4d720971063c6572e96
Paul Miyamoto <mfsaru@me.com>
2025-09-16 22:28:05 +0900

    Add Show model and link Play to Show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_16_120656) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_16_132153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_16_120656) do
     t.text "host_names"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "show_id"
+    t.index ["show_id"], name: "index_plays_on_show_id"
   end
 
+  create_table "shows", force: :cascade do |t|
+    t.integer "kexp_show_id"
+    t.string "uri"
+    t.string "program_name"
+    t.text "host_names"
+    t.datetime "airdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "plays", "shows"
 end
